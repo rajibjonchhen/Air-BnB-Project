c2d51e6039c0814a4bc305c0d94569fe6389d944
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-24 13:35:54 +0000

    footer foot added programatically

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -12,22 +12,8 @@ function App() {
 <MyNavBar/>
 <MyJumbotron/>
 <MyCards/>
-<MyFooter/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
+<MyFooter  className="App-header"/>
+   
     </div>
   );
 }
--- airbnb/src/components/MyFooter.jsx
+++ airbnb/src/components/MyFooter.jsx
@@ -2,7 +2,8 @@
 import React, { Component } from 'react';
 import {Container, Row, Col, ListGroup, ListGroupItem} from 'react-bootstrap'
 import '../App.css'
-
+import { FaFacebookF, FaTwitter, FaInstagramSquare } from "react-icons/fa";
+// import { TiSocialInstagram} from "react-icons/Ti";
 
 export default class MyFooter extends React.Component {
 
@@ -36,6 +37,16 @@ export default class MyFooter extends React.Component {
         "Investors",
         "Airbnb Luxe"],
     ]
+
+    footerFootLeft = [
+        [" © 2021 Airbnb, Inc.",
+        "·Privacy",
+        "·Terms",
+        "·Sitemap",
+        ],
+        [],
+    ]
+
     render() { 
         return (<>
                <footer>
@@ -51,6 +62,37 @@ export default class MyFooter extends React.Component {
                        </Col>
                         ))
                        }</Row>
+                       <hr/>
+                       <Row className="d-flex justify-content-between">
+                           {/* <Col> */}
+                                <ul className="list-unstyled text-left d-flex">{
+                                    this.footerFootLeft.map(list => (
+                                        <li className>{list}</li>
+                                    ))
+                                }
+                                   
+                                </ul>
+                           {/* </Col> */}
+                           {/* <Col> */}
+                                <ul className="list-unstyled text-left d-flex">
+                                    <li className = "mx-2">
+                                        English (US)
+                                    </li>
+                                    <li className = "mx-2">
+                                        € EUR
+                                    </li>
+                                    <li className = "mx-2">
+                                            <FaFacebookF/>
+                                    </li>
+                                    <li className = "mx-2">
+                                    <FaTwitter/> 
+                                    </li>
+                                    <li>
+                                    <FaInstagramSquare/>
+                                    </li>
+                                </ul>
+                           {/* </Col> */}
+                       </Row>
                        </Container> 
                </footer>
         </>);
