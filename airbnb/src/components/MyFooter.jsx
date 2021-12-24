c49b59cf8f10f2f47e0c0bad2d7c5a9bd0a32e2f
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-24 00:52:17 +0000

    footer place and created programmatically

--- airbnb/src/components/MyFooter.jsx
+++ airbnb/src/components/MyFooter.jsx
@@ -7,22 +7,52 @@ import '../App.css'
 export default class MyFooter extends React.Component {
 
     footer = [
-        [
-            
-        ],
-        [],
-        [],
-        [],
+        [  "Support",
+        "Help Center",
+        "Safety information",
+        "Cancellation options",
+        "Our COVID-19 Response",
+        "Supporting people with disabilities",
+        "Report a neighborhood concern" ],
+
+        ["Community",
+        "Airbnb.org: disaster relief housing",
+        "Support Afghan refugees",
+        "Celebrating diversity & belonging",
+        "Combating discrimination"],
+
+        ["Hosting",
+        "Try hosting",
+        "AirCover: protection for Hosts",
+        " Explore hosting resources",
+        "Visit our community forum",
+        "How to host responsibly"],
+
+        ["About",
+        "Newsroom",
+        "Learn about new features",
+        "Letter from our founders",
+        " Careers",
+        "Investors",
+        "Airbnb Luxe"],
     ]
     render() { 
         return (<>
+               <footer>
                <Container>
                    <Row>{
+                        this.footer.map(lists =>(
                        <Col>
-                       
+                       <ul className="list-unstyled text-left">{
+                           lists.map(item =>(
+                               <li className="my-3">{item}</li>
+                               ))
+                            } </ul>
                        </Col>
+                        ))
                        }</Row>
                        </Container> 
+               </footer>
         </>);
     }
 }
