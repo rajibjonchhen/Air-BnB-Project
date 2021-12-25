024e5b3d8fc18c78ccaaf245c3fcce8a002ae835
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-25 15:26:36 +0000

    jumbotron added

--- airbnb/src/components/MyJumbotron.jsx
+++ airbnb/src/components/MyJumbotron.jsx
@@ -5,18 +5,40 @@ import '../App.css'
 
 
 export default class MyJumbotron extends React.Component {
+
+    jumboStyle ={
+        color:"white",
+        top:"25%",
+        right:"0",
+        height:"100vh",
+        width:"100vw", 
+        backgroundSize: 'cover',
+        backgroundPosition:"center",
+        backgroundRepeat:"none",
+        backgroundRepeat:"none",    
+    }
+
+    buttonStyle = {
+        color:"purple",
+        borderRadius:"30px",
+        fontSize:"20px",
+        fontWeight:"600",
+        padding:"10px 20px"
+    }
+    
     render() { 
         return (<>
-                <Container>
-                <Jumbotron>
-                    <h1>Hello, world!</h1>
-                    <p>
-                        This is a simple hero unit, a simple jumbotron-style component for calling
-                        extra attention to featured content or information.
-                    </p>
-                    <p>
-                    </p>
-                    </Jumbotron>
+                <Container className="p-5" style={{backgroundColor:"black" }} fluid>
+                   <div className="position-relative">
+                    <img src= "https://a0.muscache.com/im/pictures/2459fa90-5f66-4198-a8f2-43d8f8783bc2.jpg?im_q=highq&im_w=720" style={{width:"1220px" }} />
+                  
+                   <div className="position-absolute"  style={this.jumboStyle}>      
+                    <h1>Not sure where to go? Perfect.</h1>
+                   <button style={this.buttonStyle} >
+                       I'm flexible
+                   </button>
+                   </div>
+                   </div>
                 </Container>
         </>);
     }
