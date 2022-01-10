8273b6ab0256cd7390cb497e05323b682ce7b1b1
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-10 12:11:21 +0000

    display nav white in small screen and hide black nav

--- airbnb/src/components/MyNavBar.jsx
+++ airbnb/src/components/MyNavBar.jsx
@@ -3,7 +3,7 @@ import {Navbar, Nav, NavDropdown, Container, Dropdown, Row, Col} from 'react-boo
 import {FiGlobe} from 'react-icons/fi'
 import {FaUserCircle} from 'react-icons/fa'
 import {GoThreeBars} from 'react-icons/go'
-
+import '../components/CssFiles/navbar.css'
 import brand from './brand.png'
 import '../App.css'
 
@@ -14,14 +14,14 @@ class MyNavBar extends React.Component {
     render() { 
         return (
             <Container fluid  style={{position:'fixed',zIndex:'5'}}>
-              <Row style={{padding:"0 5%",backgroundColor:"black", display:this.props.isScrolled? 'none':'block'}}>
+              <Row className="blackNav" style={{padding:"0 5%",backgroundColor:"black", display:this.props.isScrolled? 'none':'block'}}>
                 <Col>
                   <Navbar collapseOnSelect expand="lg" style={{backgroundColor:"black", }} variant="dark">
                           <Navbar.Brand href="#home"><img src="https://cdn.freebiesupply.com/images/large/2x/airbnb-logo-white-on-black.png" style={{width:"120px" }}/></Navbar.Brand>  
                           {/*red one "https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" */}
                           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                           <Navbar.Collapse className="d-flex justify-content-between" id="responsive-navbar-nav">
-                            <Nav className="ml-auto">
+                            <Nav className="hideNavLinks ml-auto">
                               <Nav.Link href="#deets" className="text-white">Become a Host</Nav.Link>
                               <Nav.Link href="#deets" ><FiGlobe/></Nav.Link>
                               <Dropdown>
@@ -45,14 +45,14 @@ class MyNavBar extends React.Component {
                 </Col>
             </Row>
             
-            <Row style={{padding:"0 5%",backgroundColor:"white", display:this.props.isScrolled? 'block':'none'}}>
+            <Row className="whiteNav" style={{padding:"0 5%",backgroundColor:"white", display:this.props.isScrolled? 'block':'none'}}>
                 <Col>
                     <Navbar collapseOnSelect expand="lg" style={{backgroundColor:"white", }} variant="dark">
                           <Navbar.Brand href="#home"><img src="https://blogravenridgecottages.files.wordpress.com/2021/04/airbnb-logo.jpeg" style={{width:"120px" }}/></Navbar.Brand>  
                           {/*red one "https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" */}
                           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                           <Navbar.Collapse className="d-flex justify-content-between" id="responsive-navbar-nav">
-                            <Nav className="ml-auto">
+                            <Nav className="hideNavLinks ml-auto">
                               <Nav.Link href="#deets" className="text-dark">Become a Host</Nav.Link>
                               <Nav.Link href="#deets" ><FiGlobe/></Nav.Link>
                               <Dropdown>
