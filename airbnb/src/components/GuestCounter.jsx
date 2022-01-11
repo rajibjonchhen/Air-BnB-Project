3c39a29d75d5988d6e7cf36b52355142fc210b51
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-11 09:09:12 +0000

    counter added

--- airbnb/src/components/GuestCounter.jsx
+++ airbnb/src/components/GuestCounter.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import {FiPlusCircle, FiMinusCircle} from 'react-icons/fi'
 import '../App.css'
-class GuestCounter extends React.Component {
+
+
+const GuestCounter = ()=>{
   
+    const [counter,setCounter]= useState(0)
     
-    render() { 
+    
         return <div className="d-flex justify-content-between align-items-center" style={{fontSize:'20px'}}>
-            <span className="m-2 pointer" onClick={(e) => this.props.guestCounter('-')}><FiMinusCircle/> </span>
-            <span className="m-2">{this.props.NumbOfGuest}</span>
-            <span className="m-2 pointer" onClick={(e) => this.props.guestCounter('+')}><FiPlusCircle/></span>
+            <span className="m-2 pointer" onClick={(e) => { counter>0 && setCounter(counter-1)}}><FiMinusCircle/> </span>
+            <span className="m-2">{counter}</span>
+            <span className="m-2 pointer" onClick={(e) => {setCounter(counter+1)}}><FiPlusCircle/></span>
         </div>;
-    }
+    
 }
  
 export default GuestCounter;
